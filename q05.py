1d101c01cec8754ff007f84d711e28165367152b
MAMEPC\heosa <heosangmin@gmail.com>
2021-07-23 21:09:33 +0900

    add q05

---
+++ q05.py
@@ -0,0 +1,27 @@
+'''
+49. Group Anagrams
+https://leetcode.com/problems/group-anagrams/
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+'''
+
+from typing import List
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+
+        return anagrams.values()
+
+solution = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(solution.groupAnagrams(strs))
