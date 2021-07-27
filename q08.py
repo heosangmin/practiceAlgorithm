f4be25913cb4f10f0b34cb477104d4be82ef3383
MAMEPC\heosa <heosangmin@gmail.com>
2021-07-28 00:18:24 +0900

    add q08

---
+++ q08.py
@@ -0,0 +1,15 @@
+'''
+42. Trapping Rain Water
+https://leetcode.com/problems/trapping-rain-water/
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+'''
+
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        pass
