38e91ef1bf6b1e13ced55c7b54d1a33eb657c90c
Sangmin Heo <heosangmin@gmail.com>
2021-07-28 15:44:41 +0900

    fix q08

--- q08.py
+++ q08.py
@@ -12,4 +12,24 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                
+                if not len(stack):
+                    break
+                
+                distance = i - stack[-1] - 1
+                waters = min( height[i], height[stack[-1]] ) - height[top]
+
+                volume += distance * waters
+            
+            stack.append(i)
+        
+        return volume
+
+solution = Solution()
+print(solution.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
