2d3c5ea0971a072ab295be7cc24e550e0e99ce5e
Sangmin Heo <heosangmin@gmail.com>
2021-10-24 17:04:45 +0900

    add q09_1

---
+++ q09_1.py
@@ -0,0 +1,54 @@
+'''
+15. 3Sum
+https://leetcode.com/problems/3sum/
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+'''
+
+from typing import List
+
+class Solution:
+    def threeSum1(self, nums: List[int]) -> List[List[int]]:
+        # 1. blute force
+        nums.sort()
+        results = []
+        for i in range(0, len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if (nums[i] + nums[j] + nums[k]) == 0 and results.count([nums[i],nums[j],nums[k]]) == 0:
+                        results.append([nums[i],nums[j],nums[k]])
+        return results
+
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        # 2. two pointer
+        nums.sort()
+        results = []
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum > 0:
+                    right -= 1
+                elif sum < 0:
+                    left += 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return results
+
+nums = [-1,0,1,2,-1,-4]
+solution = Solution()
+print(solution.threeSum1(nums))
+print(solution.threeSum2(nums))
