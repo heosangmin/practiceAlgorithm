05f3a9dc50bd748b039e1badcc2c9b48af444caa
Heo Sangmin <heosangmin@gmail.com>
2021-12-14 00:11:28 +0900

    update q42

--- tree/q42.py
+++ tree/q42.py
@@ -39,15 +39,62 @@ class Solution:
         
         depth = 0
         Q = collections.deque([root])
+        while Q:
+            depth += 1
+            for _ in range(len(Q)):
+                node = Q.popleft()
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
         
-def createTreeNodes(nodes: List):
+        return depth
+        
+def convertTreeNodes(nodes: List):
     if not nodes:
-        return []
+        return None
 
     root = TreeNode(nodes[0])
-    for i in range(1, len(nodes)):
-        pass
+    Q = collections.deque([root])
+    nodes_len = len(nodes)
 
+    for i in range(1, nodes_len+1, 2):
+        node = Q.popleft()
+        if i < nodes_len and nodes[i] is not None:
+            left = TreeNode(nodes[i])
+            node.left = left
+            Q.append(left)
+
+        if i+1 < nodes_len and nodes[i+1] is not None:
+            right = TreeNode(nodes[i+1])
+            node.right = right
+            Q.append(right)
+
+    return root
+
+def convertList(root: TreeNode):
+    Q = collections.deque([root])
+    nodes = []
+    while Q:
+        node = Q.popleft()
+        nodes.append(node.val)
+        print("root:",node.val)
+        
+        if node.left:
+            Q.append(node.left)
+            print("left:",node.left.val)
+            #nodes.append(node.left.val)
+        else:
+            nodes.append(None)
+
+        if node.right:
+            Q.append(node.right)
+            print("right:",node.right.val)
+            #nodes.append(node.right.val)
+        else:
+            nodes.append(None)
+    
+    return nodes
 
 root1 = [3,9,20,None,None,15,7]
 root2 = [1,None,2]
@@ -56,6 +103,13 @@ root4 = [0]
 
 s = Solution()
 tn1 = TreeNode(root1[0])
-#print(s.maxDepth(tn1))
 
-print(createTreeNodes(root1))
+tree_node_1 = convertTreeNodes(root1)
+tree_node_2 = convertTreeNodes(root2)
+tree_node_3 = convertTreeNodes(root3)
+tree_node_4 = convertTreeNodes(root4)
+#print(convertList(tree_node_1))
+print(s.maxDepth(tree_node_1))
+print(s.maxDepth(tree_node_2))
+print(s.maxDepth(tree_node_3))
+print(s.maxDepth(tree_node_4))
