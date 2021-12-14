d5e2bbd10fa1de226ea1cea79e722f6f5f73d34b
heos <heos@nttdata-bizsys.co.jp>
2021-12-14 19:02:54 +0900

    add q43

---
+++ tree/q43.py
@@ -0,0 +1,104 @@
+'''
+543. Diameter of Binary Tree
+https://leetcode.com/problems/diameter-of-binary-tree/
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Input: root = [1,2]
+Output: 1
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 104].
+- -100 <= Node.val <= 100
+'''
+
+from typing import Optional, List
+import collections
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def convertTreeNodes(nodes: List):
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    Q = collections.deque([root])
+    nodes_len = len(nodes)
+
+    for i in range(1, nodes_len+1, 2):
+        node = Q.popleft()
+        if i < nodes_len and nodes[i] is not None:
+            left = TreeNode(nodes[i])
+            node.left = left
+            Q.append(left)
+
+        if i+1 < nodes_len and nodes[i+1] is not None:
+            right = TreeNode(nodes[i+1])
+            node.right = right
+            Q.append(right)
+
+    return root
+
+class Solution:
+    longest: int = 0
+
+    def diameterOfBinaryTree1(self, root: Optional[TreeNode]) -> int:
+        def dfs(node: TreeNode, depth: int = 0) -> int:
+            if not node:
+                return depth
+            
+            depth_left = dfs(node.left, depth+1)
+            depth_right = dfs(node.right, depth+1)
+
+            #print(f"{node.val}: {depth_left}, {depth_right}")
+
+            return max(depth_left, depth_right)
+
+        l = dfs(root.left)
+        r = dfs(root.right)
+        depth = l + r
+
+        return depth
+
+    def diameterOfBinaryTree2(self, root: Optional[TreeNode]) -> int:
+        self.longest = 0
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return -1
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.longest = max(self.longest, left + right + 2)
+
+            return max(left, right) + 1
+        
+        dfs(root)
+        return self.longest
+        
+
+root1 = [1,2,3,4,5]
+root2 = [1,2]
+root3 = [4,-7,-3,None,None,-9,-3,9,-7,-4,None,6,None,-6,-6,None,None,0,6,5,None,9,None,None,-1,-4,None,None,None,-2]
+
+s = Solution()
+
+print(s.diameterOfBinaryTree1(convertTreeNodes(root1)))
+print(s.diameterOfBinaryTree1(convertTreeNodes(root2)))
+print(s.diameterOfBinaryTree1(convertTreeNodes(root3)))
+
+# print(s.diameterOfBinaryTree2(convertTreeNodes(root1)))
+# print(s.diameterOfBinaryTree2(convertTreeNodes(root2)))
+# print(s.diameterOfBinaryTree2(convertTreeNodes(root3)))
