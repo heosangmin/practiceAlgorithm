8dbcf7919da93cd916b62aff74feff9ec9960627
Heo Sangmin <heosangmin@gmail.com>
2022-01-13 22:07:27 +0900

    add q80

--- greedy_algorithm/q80_task-scheduler.py
+++ greedy_algorithm/q80_task-scheduler.py
@@ -42,10 +42,29 @@ import collections
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        '''
+        좌절을 안겨주는 문제다.
+        '''
         counter = collections.Counter(tasks)
         result = 0
         
-        
+        while True:
+            sub_count = 0
+            # 개수 순 추출
+            for task, _ in counter.most_common(n + 1):
+                sub_count += 1
+                result += 1
+
+                counter.subtract(task)
+                # 0 이하인 아이템을 목록에서 완전히 제거
+                counter += collections.Counter()
+
+            if not counter:
+                break
+            
+            result += n - sub_count + 1
+
+        return result
 
 
 s = Solution()
